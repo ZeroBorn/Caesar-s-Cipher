e43383131deaca616f5477471877c1d5769bb588
ZeroBorn <92119810+ZeroBorn@users.noreply.github.com>
2021-10-18 23:11:50 +0300

    Add files via upload

---
+++ Cezar/Cypher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cezar
+{
+    public class Cypher
+    {
+        public static StringBuilder encode = new StringBuilder();
+        public static StringBuilder decode = new StringBuilder();
+
+        public static string Encode(string text, int n)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+               Int32 lab =text[i] + n;
+                encode.Append(Convert.ToChar(lab));
+            }
+
+            return encode.ToString();
+        }
+
+
+        public static string Decode(string text, int n)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                
+                decode.Append(Convert.ToChar(encode[i] - n));
+            }
+            return decode.ToString();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Cezar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cezar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input text: ");
+            string text = Console.ReadLine();
+            Console.Write("Input step: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Encode: "+Cypher.Encode(text,n));
+            Console.Write("Decode: "+ Cypher.Decode(text,n));
+        }
+    }
+}
